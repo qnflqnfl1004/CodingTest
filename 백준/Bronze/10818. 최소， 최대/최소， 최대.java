3e8a928bf19aa40a4caadb4332f31c44986c8306
신영은 <102292730+qnflqnfl1004@users.noreply.github.com>
2022-04-28 18:35:27 +0900

    [Bronze III] Title: 최소, 최대, Time: 1720 ms, Memory: 317892 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int[] array = new int[n];
+		
+		
+		for (int i = 0; i < array.length; i++) {
+			array[i] = sc.nextInt();
+		}
+		
+		int max = array[0];
+		int min = array[0];
+		
+		for (int i = 0; i < array.length; i++) {
+			
+			if(array[i] > max) {
+				
+				max = array[i];
+			
+			}
+			
+			if(array[i] < min) {
+				
+				min = array[i];
+			}
+		}	
+		
+		System.out.println(min + " " + max);
+		
+	}
+}
